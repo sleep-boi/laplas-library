62c6397a5a3f152c4ace13467c706eb50f578bbe
sleep-boi <greatgleb2@gmail.com>
2026-01-19 22:36:05 +0400

    Update index.ts

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -24,6 +24,7 @@ import Comments from "./Comments"
 import Flex from "./Flex"
 import ConditionalRender from "./ConditionalRender"
 import PasswordProtection from "./PasswordProtection"
+import NewsTicker from "./NewsTicker"
 
 export {
   ArticleTitle,
